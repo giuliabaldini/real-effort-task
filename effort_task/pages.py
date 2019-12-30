b17bd8e9b464846e713f9c9ed6ed60c977ff948c
Giulia <giulia.baldini.11@gmail.com>
2019-12-30 10:53:48 +0100

    Add pages

--- effort_task/pages.py
+++ effort_task/pages.py
@@ -3,7 +3,28 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
+class Instructions(Page):
+    form_model = ['player']
+    form_fields = ['consent']
+
+    def error_message(self, values):
+        if values['consent'] == 0:
+            return "You must consent to participate in the study."
+
+
+class Assignment1(Page):
+    pass
+
+
+class Assignment2(Page):
+    pass
+
+
+class Assignment3(Page):
+    pass
+
+
+class Assignment4(Page):
     pass
 
 
@@ -16,4 +37,11 @@ class Results(Page):
     pass
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [
+    Instructions,
+    Assignment1,
+    Assignment2,
+    Assignment3,
+    Assignment4,
+    ResultsWaitPage,
+    Results]
