0e07d80fd3c593260a14ae11af5f056980a83730
Giulia <giulia.baldini.11@gmail.com>
2020-01-08 09:10:03 +0100

    Change event

--- _static/global/js/timeout.js
+++ _static/global/js/timeout.js
@@ -4,8 +4,11 @@ var hidden = parseFloat(document.getElementById("hidden_time").value) * 1000;
 var timeToReach = new Date(hidden);
 $('.timer_total').countdown(timeToReach, {elapse: true})
     .on('update.countdown', function (event) {
-        $(this).html(event.strftime('%M:%S'));
+        if (!event.elapsed) {
+            $(this).html(event.strftime('%M:%S'));
+        }
         if (event.elapsed) {
+            $(this).html(event.strftime('Expired'));
             $(":text").each(function () {
                 if ($(this).val() === "") {
                     $(this).val("NULL")
